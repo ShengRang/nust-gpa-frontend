47a8acd8a47255d305f01285ba9c0aa46770d67d
ShengRang <541628498@qq.com>
2017-10-17 16:23:41 +0800

    更新说明

--- src/compontents/about.vue
+++ src/compontents/about.vue
@@ -3,8 +3,10 @@
         <h2>关于</h2>
         <p><span class="icon powerful"></span>本项目最初仅为测试项目, 由于仍有人使用, <strong>不定期</strong>开发维护</p>
         <p><span class="icon fast"></span>避免验证码, 远离教务处, 响应快速. 自定义 GPA 计算.</p>
-        <p>由于 2016 年 4 月 1 日起 coding 平台开始收费, 如果本项目帮助了你, 请打赏以维持服务器</p>
-        <p>建议及意见, BUG 反馈, 欢迎 <a href="mailto:541628498@qq.com">联系我</a></p>
+        <p>如果本项目帮助了你, 欢迎打赏以维持服务器</p>
+        <p>代码已开源, 前端: <a href="https://github.com/ShengRang/nust-gpa-frontend">nust-gpa-frontend</a> 后端: <a href="https://github.com/NUST-MSC/NUST-API">NUST-API</a> 详解: <a href="http://shengrang.github.io/2017/03/02/nust-gpa/">Vue/Tornado 在 NUST-GPA 的实践及演进</a></p>
+        <p>欢迎贡献代码</p>
+        <p>建议及意见, BUG 反馈, 欢迎 <a href="mailto:541628498@qq.com">联系我</a> (QQ 541628498)</p>
         <strong>另外求一枚logo</strong>
         <h2>使用说明</h2>
         <p>在登录界面输入学号及教务处密码, 点击登录即可查看成绩, GPA, 均分</p>
@@ -13,6 +15,7 @@
         <p><strong>选中课程</strong>的 GPA/均分 是将<strong>选中课程(蓝底为选中)</strong>加权平均求得</p>
         <p> iOS 用户可以用 Safari 打开本站，固定到主屏幕，体验优化中</p>
         <h2>更新日志</h2>
+        <p>2017/06/&nbsp;&nbsp;&nbsp;&nbsp;毕业了</p>
         <p>2017/02/21 部分 BUG 修复</p>
         <p>2017/01/07 新增考试查询</p>
         <p>2016/11/09 前端重构</p>
