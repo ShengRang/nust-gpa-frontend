1acdbfb52409760bec2eab8de532886d0b00de36
ShengRang <541628498@qq.com>
2017-11-19 10:30:25 +0800

    修改百度统计

--- src/js/hm.js
+++ src/js/hm.js
@@ -1,8 +1,10 @@
 // 这是百度统计代码
+<script>
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?cf261137b1b39fdc2b9fa6d68bfb5027";
+  hm.src = "https://hm.baidu.com/hm.js?617a1efdb1a44e0025380eb736169be2";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+</script>
